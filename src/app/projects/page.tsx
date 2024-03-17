ace848930e68e0c9f5e15755c2fe617ccc1d78ab
Alvin Pratama <palveeen22@gmail.com>
2024-03-17 07:22:37 +0700

    feat: init project root

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,51 @@
 import React from 'react'
+import { MotionArticle, MotionDiv, MotionSection } from '@/components/MotionClient'
+import { project } from '@/data';
+
+
+
+const sectionVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+};
+
+const cardVariants = {
+    hidden: { opacity: 0, x: -100 },
+    visible: { opacity: 1, x: 0 },
+};
+
+const containerVariants = {
+    hidden: {},
+    visible: {
+        transition: {
+            staggerChildren: 0.1,
+            delayChildren: 0.2,
+        },
+    },
+};
 
 const page = () => {
     return (
-        <div>Comming soon...</div>
+        <MotionSection
+            className='w-full flex flex-col gap-14 md:gap-20 flex-grow'
+            variants={containerVariants}
+            initial="hidden"
+            animate="visible"
+        >
+            <MotionArticle
+                className='flex flex-col gap-4'
+                variants={sectionVariants}
+            >
+                <h1 className='text-5xl dark:text-foreground/100 font-bold text-pretty'>
+                    {project.title}
+                </h1>
+                <p className='text-xl break-words dark:text-foreground/80 text-pretty'>
+                    {project.pharagraph}
+                </p>
+
+            </MotionArticle>
+
+        </MotionSection>
     )
 }
 
---
+++ src/data/index.ts
@@ -0,0 +1,4 @@
+export const project = {
+    title: "Projects",
+    pharagraph: "Some things that I've created during my learning journey...."
+}
