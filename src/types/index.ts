1e72db1952d050d4b2851790bf549686487bd247
Alvin Pratama <113691381+palveeen22@users.noreply.github.com>
2025-09-17 12:34:30 +0300

    update: list project card

--- src/types/index.ts
+++ src/types/index.ts
@@ -33,3 +33,17 @@ export type TProjectDetail = {
     }
 }
 
+export type TProject = {
+    id: string;
+    slug: string;
+    url: string;
+    image: string;
+    title: string;
+    desc: string;
+    stacks: string[];
+    keywords: string[];
+    features: string[];
+    architectureHighlights: string[];
+    metaTitle: string;
+    metaDescription: string;
+}
